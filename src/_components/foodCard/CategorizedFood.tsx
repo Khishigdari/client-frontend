c3c6a623170d846703285e02c69d42c63c907205
Khishigdari <nbhishgee22@gmail.com>
2025-10-29 16:59:36 +0800

    trial to change the width of the food detail dialog10.29

--- src/_components/foodCard/CategorizedFood.tsx
+++ src/_components/foodCard/CategorizedFood.tsx
@@ -105,80 +105,82 @@ export const CategorizedFood = ({
                     </div>
                   </div>
                 </DialogTrigger>
-                <DialogTitle></DialogTitle>
-                <DialogContent className="max-w-207">
-                  {selectedFood && (
-                    <div className="" key={selectedFood._id}>
-                      <div className="flex p-6 gap-6 w-full">
-                        <div>
-                          <img
-                            src={selectedFood.image}
-                            className="rounded-xl h-91 w-94"
-                          />
-                        </div>
-                        <div className="pt-9 flex flex-col justify-between">
-                          <div className="flex flex-col gap-3">
-                            <h2 className="text-[30px] leading-9 font-[600] text-red-500">
-                              {selectedFood.name}
-                            </h2>
-                            <p className="text-4 leading-6 font-[400] text-foreground">
-                              {selectedFood.ingredients}
-                            </p>
+                <DialogContent>
+                  <DialogTitle></DialogTitle>
+                  <DialogContent className="w-207">
+                    {selectedFood && (
+                      <div className="" key={selectedFood._id}>
+                        <div className="flex p-6 gap-6 w-full">
+                          <div>
+                            <img
+                              src={selectedFood.image}
+                              className="rounded-xl h-91 w-94"
+                            />
                           </div>
-                          <div className="flex flex-col gap-6">
-                            <div className="flex justify-between">
-                              <div className="flex flex-col gap-0">
-                                <p className="text-foreground text-4 leading-6 font-[400]">
-                                  Total price
-                                </p>
-                                <h3 className="text-foreground text-6 leading-8 font-[600]">
-                                  ${selectedFood.price * quantity}
-                                </h3>
-                              </div>
-                              <div className="flex gap-3 items-center">
-                                <Button
-                                  variant={"outline"}
-                                  className="rounded-full "
-                                  size={"lg"}
-                                  onClick={handleCountDownOnClick}
-                                  disabled={quantity === 1}
-                                >
-                                  <Minus />
-                                </Button>
-                                <p className="text-[18px] leading-7 font-[600] text-foreground">
-                                  {quantity}
-                                </p>
-                                <Button
-                                  variant={"outline"}
-                                  className="rounded-full "
-                                  size={"lg"}
-                                  onClick={handleCountUpOnClick}
-                                >
-                                  <Plus />
-                                </Button>
-                              </div>
+                          <div className="pt-9 flex flex-col justify-between">
+                            <div className="flex flex-col gap-3">
+                              <h2 className="text-[30px] leading-9 font-[600] text-red-500">
+                                {selectedFood.name}
+                              </h2>
+                              <p className="text-4 leading-6 font-[400] text-foreground">
+                                {selectedFood.ingredients}
+                              </p>
                             </div>
-                            <Button
-                              className="rounded-full py-2 px8 w-94 text-[14px] leading-5 font-[500]"
-                              onClick={() =>
-                                toast.success(
-                                  "Food is being added to the cart!"
-                                  //   {
-                                  //   style: {
-                                  //     background: "primary",
-                                  //     border: "1px",
-                                  //   },
-                                  // }
-                                )
-                              }
-                            >
-                              Add to cart
-                            </Button>
+                            <div className="flex flex-col gap-6">
+                              <div className="flex justify-between">
+                                <div className="flex flex-col gap-0">
+                                  <p className="text-foreground text-4 leading-6 font-[400]">
+                                    Total price
+                                  </p>
+                                  <h3 className="text-foreground text-6 leading-8 font-[600]">
+                                    ${selectedFood.price * quantity}
+                                  </h3>
+                                </div>
+                                <div className="flex gap-3 items-center">
+                                  <Button
+                                    variant={"outline"}
+                                    className="rounded-full "
+                                    size={"lg"}
+                                    onClick={handleCountDownOnClick}
+                                    disabled={quantity === 1}
+                                  >
+                                    <Minus />
+                                  </Button>
+                                  <p className="text-[18px] leading-7 font-[600] text-foreground">
+                                    {quantity}
+                                  </p>
+                                  <Button
+                                    variant={"outline"}
+                                    className="rounded-full "
+                                    size={"lg"}
+                                    onClick={handleCountUpOnClick}
+                                  >
+                                    <Plus />
+                                  </Button>
+                                </div>
+                              </div>
+                              <Button
+                                className="rounded-full py-2 px8 w-94 text-[14px] leading-5 font-[500]"
+                                onClick={() =>
+                                  toast.success(
+                                    "Food is being added to the cart!"
+                                    //   {
+                                    //   style: {
+                                    //     background: "primary",
+                                    //     border: "1px",
+                                    //   },
+                                    // }
+                                  )
+                                }
+                              >
+                                Add to cart
+                              </Button>
+                            </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  )}
+                    )}
+                  </DialogContent>
                 </DialogContent>
               </Dialog>
             </div>
